b0735f210031b94ea433ff39044630d5885b2c43
Ayush Agrawal <ayushnita.ece@gmail.com>
2020-10-24 11:27:10 +0530

    Add basic tools and Software

---
+++ setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Basic Command for updating Ubuntu 
+
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get autoclean
+sudo apt-get autoremove
+
+
+# Install Terminal Softwares 
+
+sudo apt-get install vim tmux curl python3-pip
+
+
+# Install youtube downloading tools 
+
+pip3 install youtube-downloading
+
+#VSCode Instalation 
+
+sudo snap install code --classic
+
+#VLC for video Editing
+
+sudo snap install vlc
+
+# PostMan
+
+sudo snap install postman
+
+# Mail 
+
+sudo snap install mailspring
+
+# STS
+
+
+
+#JAVA
+sudo apt-get install openjdk-8-demo openjdk-8-jre openjdk-8-jdk
