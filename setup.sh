d1a97f3539e2b2276c7504bfe7c9c8fb2ad1abf2
Ayush Agrawal <ayushnita.ece@gmail.com>
2020-10-25 14:56:06 +0530

    Add TODO and some more tools

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ sudo apt-get autoremove
 
 # Install Terminal Softwares 
 
-sudo apt-get install vim tmux curl python3-pip
+sudo apt-get install vim tmux curl python3-pip dos2unix
 
 
 # Install youtube downloading tools 
@@ -40,3 +40,12 @@ sudo snap install mailspring
 
 #JAVA
 sudo apt-get install openjdk-8-demo openjdk-8-jre openjdk-8-jdk
+
+#Zoom 
+
+sudo snap install zoom-client
+
+#Skype 
+
+sudo snap install skype --classic
+
